09328e32223aaf4f39f8f6ed3d69763035aa7b10
pierreferrette <58181582+pierreferrette@users.noreply.github.com>
2019-11-28 11:47:39 +0100

    Update EbureauApplication.java

--- src/main/java/com/formation/ebureau/EbureauApplication.java
+++ src/main/java/com/formation/ebureau/EbureauApplication.java
@@ -1,6 +1,6 @@
 package com.formation.ebureau;
 
-import org.springframework.boot.SpringApplication
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
